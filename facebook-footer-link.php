925eff86d06073be25a9a25c9810737beae28000
kirandash <thekirankumardash@gmail.com>
2016-09-06 20:27:24 +0530

    003 Wordpress Plugin Initial Setup

---
+++ facebook-footer-link.php
@@ -0,0 +1,15 @@
+<?php
+/**
+*
+* Plugin Name: Facebook Footer Link
+* Description: Adds a Facebook profile link to the end of posts
+* Version: 1.0
+* Author: Kiran Dash
+**/
+
+// Exit if Accessed Directly
+if( !defined('ABSPATH') ){
+	exit;
+}
+
+?>
